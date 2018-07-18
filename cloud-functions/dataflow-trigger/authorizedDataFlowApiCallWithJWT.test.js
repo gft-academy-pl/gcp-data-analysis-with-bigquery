387375efef2aaf72f07416479bff057be8c9fdc4
Jedrzej Nowowiejski <jjni@gft.com>
2018-07-18 11:43:44 +0100

    Add cloud function

---
+++ cloud-functions/dataflow-trigger/authorizedDataFlowApiCallWithJWT.test.js
@@ -0,0 +1,25 @@
+const assert = require('assert');
+const index = require('./index');
+const { google } = require('googleapis');
+const path = require('path');
+const fs = require("fs");
+const CONFIG = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json')));
+
+google.auth.getClient({
+	keyFile: path.join(__dirname, 'jwt.keys.json'),
+	scopes: [
+		'https://www.googleapis.com/auth/cloud-platform',
+		'https://www.googleapis.com/auth/userinfo.email'
+	]
+}).then(auth => {
+	return google.auth.getDefaultProjectId().then(projectId => {
+		console.log('ProjectId', projectId);
+		return index.createJob(auth, projectId, {
+			bucket: CONFIG.INPUT_BUCKET,
+			name: 'trades2016.csv'
+		}).then(result => {
+			assert.equal(result.data.type, 'JOB_TYPE_BATCH');
+			console.log('result', result.data);
+		}, err => console.log(err));
+	});
+});
---
+++ cloud-functions/dataflow-trigger/index.js
@@ -0,0 +1,63 @@
+const { google } = require('googleapis');
+const fs = require("fs");
+const path = require('path');
+
+const CONFIG = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json')));
+
+exports.triggerDataflowFn = (event, callback) => {
+
+	console.log('Processing incomming event');
+
+	const file = event.data;
+
+	if (file.name.startsWith('trades_')) {
+      return google.auth.getClient({
+		scopes: [
+			'https://www.googleapis.com/auth/cloud-platform',
+			'https://www.googleapis.com/auth/userinfo.email'
+		]
+		}).then((auth) => {
+			return google.auth.getDefaultProjectId()
+				.then((projectId) => {
+					console.log(`Succesfully authorized with JWT key, project: ${projectId}`);
+					return exports.createJob(auth, projectId, file);
+				}, (err) => {
+					console.log('Error during obtaining projectId', err);
+					return err;
+				});
+
+		}, (err) => {
+			console.log('Error during authorization', err);
+			return err;
+		});
+	} else {
+		callback();
+	}
+};
+
+exports.createJob = function(auth, projectId, file) {
+	const params = {
+		projectId: projectId,
+		resource: {
+			parameters: {
+                inputLocations: `{"location1":"gs://${CONFIG.INPUT_BUCKET}/${file.name}"}`,
+				customGcsTempLocation: `gs://${CONFIG.TEMP_BUCKET}`,
+				outputLocations: `{"location1":"${CONFIG.OUTPUT_LOCATION}"}`
+			},
+			jobName: 'dataprep-load-trades-' + new Date().toISOString(),
+			gcsPath: `gs://${CONFIG.TEMPLATE_LOCATION}`
+		}
+	};
+	console.log('Requesting params: ', params);
+	return google.dataflow({
+			version: 'v1b3',
+			auth: auth
+		})
+		.projects.templates.create(params).then((result) => {
+			console.log('OK');
+			return result;
+		}, (err) => {
+			console.log('Error during executing call', err);
+			return err;
+		});
+};
